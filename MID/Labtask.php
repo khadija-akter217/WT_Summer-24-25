434d8d7e6a541386f4f73314e360474bfaef4521
Khadija Akter <ayeshanur2000@gmail.com>
2025-07-20 10:47:01 +0600

    Add files via upload

---
+++ MID/Labtask.php
@@ -0,0 +1,67 @@
+</DOCTYPE html>
+<html>
+    <title> This is a registration form</title>
+    <body>
+        <center>
+         <h1 style="color : blue;">AIUB</h1>
+         <h2 style="color : blue;">Registration Form</h2>
+         </center>
+
+         <h3 align="left">Complete the Registration</h3>
+
+         <table>
+            <tr> <td>
+         Full Name</td><td><input type = "text"></td>
+</tr>
+
+ <tr> <td>
+         Email</td><td><input type = "text"></td>
+</tr>
+
+ <tr> <td>
+         Password</td><td><input type = "text"></td>
+</tr>
+ <tr> <td>
+         Gender</td>
+         <td><input type = "radio" name="des">Male</td>
+          <td><input type = "radio" name="des">Other</td>
+</tr>
+<tr> <td>
+         Language Known</td>
+         <td><input type = "checkbox" name="des">English</td>
+          <td><input type = "checkbox" name="des">Bangla</td>
+          <td><input type = "checkbox" name="des">Hindi</td>
+</tr>
+
+         <tr>
+            <td>Country:</td>
+        <td>
+         <select name = "Country">
+         <opion value = "--Select--"></option>
+         <opion value = "Africa">Africa</option>
+         <opion value = "Bangladesh">Bangladesh</option>
+         <opion value = "Canada">Canada</option>
+         <opion value = "India">India</option>
+         <opion value = "Pakistan">Pakistan</option>
+</td>
+</tr>
+
+
+<tr> <td>
+         Select Your Date</td><td><input type = "date"></td>
+</tr>
+<tr> <td>
+         Upload photo</td><td><input type = "file"></td>
+</tr>
+
+<tr> <td>
+         Comment</td><td><input type = "text"></td>
+</tr>
+
+<tr> <td>
+     <input type = "Submit"></td>
+</tr>
+
+
+</body>
+</html>
