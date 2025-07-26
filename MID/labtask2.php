b9ddd9f8023ea57dcf83ba7b6c8c3130a81da227
Khadija Akter <ayeshanur2000@gmail.com>
2025-07-26 10:57:04 +0600

    Add files via upload

---
+++ MID/labtask2.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+    <title>bank management system</title>
+    <body>
+    <center>
+            <h1 style="color:blue">Bank Management System</h1>
+            <h2 style="color:blue">Your Trusted Financial Partner</h2>
+    </center>
+     <h3 align="left">Customer Registration Form</h3>
+
+         <table>
+            <tr> <td>
+         Full Name:</td><td><input type = "text"></td>
+</tr>
+
+<tr> <td>
+         Date of Birth:</td><td><input type = "date"></td>
+</tr>
+
+ <tr> <td>
+         Gender</td>
+         <td><input type = "radio" name="des">Male</td>
+         <td><input type = "radio" name="des">Female</td>
+          <td><input type = "radio" name="des">Other</td>
+</tr>
+
+ <tr>
+            <td>Marital Status:</td>
+        <td>
+         <select name = "Marital Status">
+         <opion value = "Single">Single</option>
+         <opion value = "Married">Married</option>
+         <opion value = "Widow">Widow</option>
+</td>
+</tr>
+
+<tr>
+            <td>Account Type:</td>
+        <td>
+         <select name = "Marital Status">
+        
+         <opion value = "Savings">Savings</option>
+         <opion value = "Current">Current</option>
+</td>
+</tr>
+
+ <tr> <td>
+         Initial Deposite Amount:</td><td><input type = "text"></td>
+</tr>
+
+ <tr> <td>
+         Email Address:</td><td><input type = "text"></td>
+</tr>
+
+ <tr> <td>
+         Address:</td><td><input type = "text"></td>
+</tr>
+
+<tr> <td>
+         Occupation:</td><td><input type = "text"></td>
+</tr>
+
+<tr> <td>
+         National ID (NID):</td><td><input type = "text"></td>
+</tr>
+
+<tr> <td>
+         Set password:</td><td><input type = "text"></td>
+</tr>
+      
+<tr> <td>
+         Upload ID Proof:</td><td><input type = "file"></td>
+</tr>
+
+<tr> <td><input type = "checkbox" name="des">I agree to the terms and conditions</td>
+</tr>
+
+<tr> <td>
+     <input type = "Submit" name="des"></td>
+</tr>
+
+</body>
+
+<style>
+    div{
+        background-color: skyblue;
+        width: 200px;
+        height: 65px;
+        border: 1px solid black;
+        overflow: scroll;
+    }  
+
+    h1,h2{<color: darkblue;}
+    body{
+        background-color: skyblue;
+
+    }
+    table{
+        background-color: white;
+        border: 2px solid darkblue;
+        padding: 20px;
+        margin: 20px;
+    }
+ </style>
+
+    <div>This is a demo text to show how overflow works in a small container with a text</div>
+
+</html>
